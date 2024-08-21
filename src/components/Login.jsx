d91fe008e9f98e7a0e8de0b6c9e73e9c4f49047d
Agleice Faria de Sousa <agleicesousa98@gmail.com>
2024-08-20 21:06:15 -0300

    feat: add corpo html do login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,43 @@
+import logoPadrao from '../assets/images/logo-padrao.png';
+import logoBranca from '../assets/images/logo-branca.png';
+import background from '../assets/videos/background.mp4';
 
 const Login = () => {
 
     return (
         <div>
-            <h1>hello, World!</h1>
+            <div>
+                {/* Seção Esquerda do Login */}
+                <div>
+                    <img src={logoPadrao} alt="Logo Geração Tech" />
+                    <h4>Acesso ao EAD do <strong>Geração Tech</strong></h4>
+                    <p>Preencha os seus dados abaixo.</p>
+                    <form method="post" action="moodle_login.php">
+                        <input type="text" id="cpf" placeholder="CPF" required maxLength="11" name="username"/>
+                        <input type="password" placeholder="Senha" required maxLength="30" name="password"
+                        />
+                        <div>
+                            <label htmlFor="rememberusername">
+                                <input type="checkbox" name="rememberusername" id="rememberusername" value="1"/>
+                                Lembrar meu acesso
+                            </label>
+                            <a href="/"> Esqueceu a senha? </a>
+                        </div>
+                        <button type="submit"> Acessar </button>
+                    </form>
+                </div>
+
+                {/* Seção Direita do Login */}
+                <div>
+                    <video>
+                        <source src={background} type="video/mp4" />
+                        Seu navegador não suporta a tag de vídeo.
+                    </video>
+                    <div></div>
+                    <img src={logoBranca} alt="Logo Branca"
+                    />
+                </div>
+            </div>
         </div>
     );
 };
